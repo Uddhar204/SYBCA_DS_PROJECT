38b0322f9170b46786b0ba2731dd1408eabcdd3d
Uddhar204 <91396546+Uddhar204@users.noreply.github.com>
2025-08-31 19:25:17 +0530

    assignment 1

---
+++ program_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    {int ar [11];
+       int num=0;
+    for (int i = 0; i < 11; i++) 
+    {if(num%2==0) 
+    {ar[i] = num;
+            num += 2;
+    }
+    }
+    for (int i = 0; i < 11; i++) {
+        cout << ar[i] << " ";
+    }
+}
+    return 0;
+}
